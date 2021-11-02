6a9bde2858a7af105da7ba0e947cc7b6108e6be5
papadorsey <89880323+papadorsey@users.noreply.github.com>
2021-11-01 20:34:32 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,36 @@
+function setup()
+{
+    createCanvas(1900, 950);
+}
+function draw()
+{
+    background(12,50,18);
+    fill(255, 229, 180);
+    circle(950,500,800);
+
+    fill(23,48,183);
+    circle(800,400,100);
+    
+    fill(0,0,0);
+    circle(800,400,50);
+
+    fill(23,48,183);
+    circle(1100,400,100);
+
+    fill(0,0,0)
+    circle(1100,400,50)
+
+    fill(235, 181, 184)
+    ellipse(950, 700, 350, 30)
+    ellipse(950, 675, 350, 45)
+
+    fill(150,50,20)
+    rect(925,480,60,150)
+
+    rect(1040,280,150,50)
+    rect(725,280,150,50)
+
+    fill(150, 75, 0)
+    arc(950, 300, 698, 400, radians(180), radians(360));
+    
+}
